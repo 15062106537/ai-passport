9d6d8c2253ee9f8f036d0d695d0f8ec70a86ac7d
15062106537 <1922130870@qq.com>
2026-08-28 15:57:36 +0800

    update main/demo.h

--- main/demo.h
+++ main/demo.h
@@ -34,7 +34,7 @@ void demo_ble_key(bsp_btn_t btn, bsp_btn_ev_t ev);
 void demo_low_power_enter(void); void demo_low_power_exit(void);
 void demo_low_power_key(bsp_btn_t btn, bsp_btn_ev_t ev);
 
-bool demo_voice_enter(void);    void demo_voice_exit(void);
+void demo_voice_enter(void);    void demo_voice_exit(void);
 void demo_voice_key(bsp_btn_t btn, bsp_btn_ev_t ev);
 
 void demo_dice_enter(void);     void demo_dice_exit(void);
